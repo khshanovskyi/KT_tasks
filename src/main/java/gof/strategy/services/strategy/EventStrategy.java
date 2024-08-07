ca0025359fe445f997f7f6d05de861801c97b501
khshanovskyi <Pavlo_Khshanovskyi@epam.com>
2024-08-07 21:50:58 +0300

    Add GoF Strategy task

--- src/main/java/gof/strategy/services/strategy/EventStrategy.java
+++ src/main/java/gof/strategy/services/strategy/EventStrategy.java
@@ -1,11 +1,14 @@
 package gof.strategy.services.strategy;
 
+import gof.strategy.domain.EventResourceType;
+import gof.strategy.domain.EventType;
 import gof.strategy.domain.ScheduledEvent;
+import org.apache.commons.lang3.tuple.Pair;
 
 public interface EventStrategy {
 
     ScheduledEvent process(ScheduledEvent event);
 
-//    Pair<EventType, EventResourceType> getType();
+    Pair<EventType, EventResourceType> getType();
 
 }
