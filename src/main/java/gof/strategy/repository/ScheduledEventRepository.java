021931f227f59cab4fede5e79170e7f3b25700b5
khshanovskyi <Pavlo_Khshanovskyi@epam.com>
2024-08-07 21:10:36 +0300

    Add GoF Strategy task

--- src/main/java/gof/strategy/repository/ScheduledEventRepository.java
+++ src/main/java/gof/strategy/repository/ScheduledEventRepository.java
@@ -5,22 +5,23 @@ import gof.strategy.domain.ScheduledEvent;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 public class ScheduledEventRepository {
 
     private final Map<UUID, ScheduledEvent> scheduledEvents = new HashMap<>();
 
-    public void add(ScheduledEvent scheduledEvent) {
-        scheduledEvents.put(scheduledEvent.getId(), scheduledEvent);
+    public ScheduledEvent save(ScheduledEvent scheduledEvent) {
+        return scheduledEvents.put(scheduledEvent.getId(), scheduledEvent);
     }
 
     public List<ScheduledEvent> getAll() {
         return scheduledEvents.values().stream().toList();
     }
 
-    public ScheduledEvent update(ScheduledEvent event) {
-        scheduledEvents.put(event.getId(), event);
-        return event;
+    public Optional<ScheduledEvent> getById(UUID id) {
+        return Optional.ofNullable(scheduledEvents.get(id));
     }
+
 }
